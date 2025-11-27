ff3a30d4bd108bb74ae107b453c959f4077640d9
jkind889 <jova.kind@bison.howard.edu>
2025-11-27 15:04:45 -0500

    Addition - Added Remove Button and Hover logic
    Added remove button to clear the grid as well as the logic
    so the grid can reset when the button is pressed

--- sketch.js
+++ sketch.js
@@ -9,9 +9,32 @@ for(let i = 0; i<16;i++)
     for(let j = 1; j<=16;j++)
     {
         let row = document.createElement("div")
+        row.classList.add("row");
+        row.textContent = (i*16)+j;
         column.appendChild(row);
     }
     screen.appendChild(column)
 }
 
-// i should 
+screen.addEventListener("mouseover",(e) =>
+{
+    //our target is the row divs
+    if(e.target.classList.contains("row"))
+    {
+        e.target.style.backgroundColor = "black";
+    }
+});
+
+let remove = document.querySelector(".remove")
+
+
+remove.addEventListener("click",() =>
+{
+    let rows = document.querySelectorAll(".row");
+    rows.forEach(square =>
+    {
+        square.style.backgroundColor = "white";
+    });
+
+
+});
